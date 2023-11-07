ae8a6c038c24f30d2c214502fe7727d013a8ae86
Lucas Habegger <lucas.habegger@biognosys.com>
2023-11-08 00:03:41 +0100

    fixed nuxt.config.tx for build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  build: { transpile: ["@fawmi/vue-google-maps"] },
+  build: { transpile: ["vue-google-maps-community-fork", '@googlemaps/markercluster'] },
   modules: [
     "@nuxtjs/tailwindcss",
   ],
